9b18683b299edb57d337b4fb661b746692c4fca3
Richard Bonichon <richard.bonichon@gmail.com>
2017-04-21 19:50:35 +0200

    [design] general update

--- assets/js/issisp.js
+++ assets/js/issisp.js
@@ -20,4 +20,20 @@
             top: 100
         }});
 
+  var offset = 250;
+  var duration = 300;
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > offset) {
+      $(".back-to-top").fadeIn(duration);
+    } else {
+      $(".back-to-top").fadeOut(duration);
+    }
+  });
+
+  $(".back-to-top").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({scrollTop: 0}, duration);
+    return false;
+  });
+
 })(jQuery); // End of use strict
